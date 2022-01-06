eb99a0c3226e874af513e6bbec644b8b91de69c4
Nwankwo Kelechi <84792410+NwankwoKc@users.noreply.github.com>
2022-01-06 00:25:52 -1100

    Add files via upload

---
+++ Verification/application/javascript.js
@@ -0,0 +1,15 @@
+var username = document.getElementById("name");
+var emailid = document.getElementById("email");
+function checkdata(){
+    if(username.value == ""){
+        alert("Please input your username");
+        username.focus()
+        return false
+    }
+    if(emailid.value == ""){
+        alert("Please input your E-mail");
+        emailid.focus()
+        return false
+    }
+    return alert("you have succesfully sent your details");
+};
